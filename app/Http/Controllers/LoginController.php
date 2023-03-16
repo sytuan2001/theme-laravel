24b2a42b9fa126ae35811f638425925bfb6da4fe
sytuan2001 <thuancat1978@gmail.com>
2023-03-16 10:45:16 +0700

    add logout

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -1,29 +1,27 @@
 <?php
- 
+
 namespace App\Http\Controllers;
- 
+
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
- 
+use Illuminate\Support\Facades\Session;
+
 class LoginController extends Controller
 {
-    /**
-     * Handle an authentication attempt.
-     */
     public function authenticate(Request $request): RedirectResponse
     {
         $credentials = $request->validate([
             'email' => ['required', 'email'],
             'password' => ['required'],
         ]);
- 
+
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
- 
-            return redirect()->intended('dashboard');
+
+            return redirect()->intended('/');
         }
- 
+
         return back()->withErrors([
             'email' => 'The provided credentials do not match our records.',
         ])->onlyInput('email');
--- routes/web.php
+++ routes/web.php
@@ -2,45 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
-use App\Http\Controllers\UserAuth;
-
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-// Route::post("user",[UserAuth::class,'userLogin']);
-
-// Route::get('/login', function () {
-//     return view('Login.login');
-// });
+use App\Http\Controllers\LogoutController;
 
 Route::get('/admin', function () {
     return view('admin');
 });
 
-Route::group(['middleware' => 'guest'], function() {
+Route::group(['middleware' => 'guest'], function () {
     Route::get('/login', function () {
         return view('Login/login');
     })->name('login');
-    Route::post('/login', [LoginController::class,'authenticate'] )->name('login_post');
+    Route::post('/login', [LoginController::class, 'authenticate'])->name('login_post');
 });
 
-Route::group(['middleware' => 'auth'], function() {
+Route::group(['middleware' => 'auth'], function () {
     Route::get('/', function () {
         return view('dashboard.content');
     })->name('dashboard');
+    Route::get('/logout', [LogoutController::class, 'logout'])->name('logout');
 });
-
-Route::get('/something', function () {
-    return redirect('/admin');
-});
-// Route::get('home', [
-//     HomeController::class,
-//     'index'
-// ]);
-// Route::get('/login', 'LoginController@login');
-
-//Route::get('/home', 'dashboard@content');
-
