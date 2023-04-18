4792e0b4b73da2fb2d2954b58b15e00f42783445
sytuan2001 <thuancat1978@gmail.com>
2023-04-19 01:12:09 +0700

    add

--- resources/views/todos/home.blade.php
+++ resources/views/todos/home.blade.php
@@ -50,7 +50,7 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="taskModalLabel">Task Detail</h5>
+                    <h5 class="modal-title" id="taskModalLabel">Nội Dung Chi Tiết</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
